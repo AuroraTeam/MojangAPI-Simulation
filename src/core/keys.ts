9191d78e59febc00b4c785c52662b8327754a700
Andrew Molchanov <mr.catmanrus@gmail.com>
2021-06-09 00:43:53 +0300

    Мелкие правки

--- src/core/keys.ts
+++ src/core/keys.ts
@@ -4,9 +4,11 @@ import path from "path";
 
 const keysDir = path.join(__dirname, "../../keys");
 const privateKeyPath = path.join(keysDir, "private.pem");
+const publicKeyPath = path.join(keysDir, "yggdrasil_session_pubkey.der");
+const privateKey = fs.readFileSync(privateKeyPath);
 
 export function generateKeys(): void {
-    if (fs.existsSync(privateKeyPath))
+    if (fs.existsSync(privateKeyPath) && fs.existsSync(publicKeyPath))
         return console.log("Keys exists, skip generate");
 
     if (!fs.existsSync(keysDir)) fs.mkdirSync(keysDir);
@@ -24,10 +26,7 @@ export function generateKeys(): void {
 
     fs.writeFileSync(privateKeyPath, keys.privateKey);
     console.log("Private key saved");
-    fs.writeFileSync(
-        path.join(keysDir, "yggdrasil_session_pubkey.der"),
-        keys.publicKey
-    );
+    fs.writeFileSync(publicKeyPath, keys.publicKey);
     console.log("Public key saved");
 }
 
@@ -36,5 +35,5 @@ export function getSignature(data: string) {
     const sign = crypto.createSign("RSA-SHA1");
     sign.update(data);
     sign.end();
-    return sign.sign(fs.readFileSync(privateKeyPath), "base64");
+    return sign.sign(privateKey, "base64");
 }
